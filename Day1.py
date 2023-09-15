4105533c902a4f3915da1d729c997008291749bf
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-09-15 22:05:43 +0530

    it is about how to print, how to convert string to int and add , type().

---
+++ Day1.py
@@ -0,0 +1,24 @@
+# print("hello world")
+# for comment we use hastag for single line and for multi line comment we use triple """ .
+# print("HII my name is ", end="")   # end is new line character
+print("Nidhi Gupta")
+print("HII my name is","Nidhi Gupta", end=" ")
+print("How are you?")
+print("c:\"harry\" ")
+
+var1 = "nidhi gupta "
+var2 = 4
+var3 = 35.6
+var4 = "hii"
+var5 = "25 "
+var6 = "25"
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var4  + var1)
+print(int(var5) + int(var6))
+print(var5 + var6)
+print(5 * var1)  #5 multiply with var1 means it will print 5 times.
+print(5 * "nidhi \n")
+print(5 * int(var5) + int(var6))
+print(5 * str(int(var5) + int(var6)))  #integr value to string.
