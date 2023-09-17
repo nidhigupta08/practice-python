7a43ab1010d5c520e026bddd1c49ff9321d26934
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-09-17 21:53:40 +0530

    how to take input from user and convert to int and Additingthose two value.

---
+++ Day2.py
@@ -0,0 +1,23 @@
+print("Enter a number:")
+num = input()
+print("you have entered", int(num) +10)
+
+#Addition of two number.
+
+print("Enter num1:")
+num1=input()
+print("enter num2: ")
+num2=input()
+result=int(num1 ) +int(num2)
+print(result)
+
+ #             OR
+print("Enter num1:")
+num1 = int(input())
+print("enter num2: ")
+num2 = int(input())
+result = num1 + num2
+print(result)
+
+
+
