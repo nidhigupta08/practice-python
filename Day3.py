d34ad92d80fd1462f407a1107cf446d5999636cb
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-09-20 21:49:43 +0530

    how to find age of anyone. Printing using 'f' without using concatenation (+).

---
+++ Day3.py
@@ -0,0 +1,13 @@
+name="John Smith"
+age=20
+is_new=True
+
+name=input("enter a your name: ")
+fav_color=input("WHt is your favriote color?")
+print(name + " likes " +fav_color )
+
+birth_year=int(input("enter the birth year"))
+current_year=int(input("enter the current year"))
+result=(current_year-birth_year)
+print(f"my name is Nidhi Gupta and i m {result} years old")
+
