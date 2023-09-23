9eee18e4ef8648b34c8599a2038bb4b18646fc83
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-09-23 16:34:28 +0530

    All about List and its methods.  Write a program to remove the duplicate in alist.  Write a programs to find the largest number in a list

--- List.py
+++ List.py
@@ -1,7 +1,9 @@
 #LIST -it can take any data type .15
 grocery = ["rice", "Vim bar", "Deo", "pulses", 8]
-#print (grocery)
+print (grocery)
 num = [20, 44, 1, 99, 2, 4, 6, 8, 10]
+result=max(num)
+print(result)
 print(num[3])
 num.sort()
 print(num)
@@ -25,7 +27,7 @@ print(names[: :]) #print all
 names[0]='jon'
 print(names)
 
-#Write a programs to find the largest number in a list
+ #Write a programs to find the largest number in a list
 
 numbers=[3,6,1,2,8,1,9,6,3,5,12]
 max=numbers[0]
@@ -33,3 +35,32 @@ for num in numbers:
     if num>max:
         max=num
 print(max)
+
+#2 dimentinal list
+matrix=[
+
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+matrix[1][2]=12
+print(matrix[1][2])
+
+for row in matrix: #  [1,2,3], n so on
+    for item in row:  #  items of one list [1,2,3]
+        print(item)
+
+# Write a program to remove the duplicate in alist.
+
+num1 = [20, 44, 1, 99, 2, 4, 6, 8, 10,44,20]
+print(set(num1))
+
+#           OOOOOOOOOOOOOOOOOOOORRRRRRRRRRRRRRRRRRRRRRR
+uniques=[]
+for number in num1:
+    if number not in uniques:
+        uniques.append(number)
+
+print(uniques)
+
