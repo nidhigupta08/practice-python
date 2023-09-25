c9ed9ccb3a038f64da35d588caa7b37ea5794d37
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-09-25 23:44:22 +0530

    Round the result of the division 10/3 to a number with 2 decimal places, and print the rounded value on the screen.  Round the number 10.676767 to the nearest integer, and print the result.  Calculate the square root of 5, and print the result rounded to 4 decimal places.

---
+++ practice.py
@@ -0,0 +1,15 @@
+"""Round Practice #1
+ Round the result of the division 10/3 to a number with 2 decimal places, and print the rounded value on the screen"""
+value = round(10 / 3, 2)
+print(value)
+
+"""Round Practice #2
+Round the number 10.676767 to the nearest integer, and print the result."""
+num = 10.676767
+result = round(num)
+print(result)
+
+'''Round Practice #3
+Calculate the square root of 5, and print the result rounded to 4 decimal places.'''
+x = 5
+print(f"{x ** 0.5:.4f}")
