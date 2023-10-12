fa03fc71194d8ec3be741a71e2efd61e758d868f
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-10-12 19:56:37 +0530

    program to check the user input is even or not .

---
+++ my_pac/print_even.py
@@ -0,0 +1,10 @@
+# for i in range(10):
+#     print(2*i)
+
+def check_even(number):
+    if(number%2==0):
+        print("Number is even 😊")
+    else:
+        print("Number is not even😑")
+num=int(input("enter your number :"))
+check_even(num)
