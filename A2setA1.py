00aedcea450d40adfd765a4b02f20f2eef42653a
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-10-21 18:25:25 +0530

    college_time python

---
+++ A2setA1.py
@@ -0,0 +1,19 @@
+
+
+
+# print('Enter 5 numbers..')
+# a = list()
+# for i in range(5):
+#     a.append(int(input('Enter: ')))
+#     if len(set(a))!= len(a):
+#         print('DUPLICATES.')
+#     else:
+#         print('Unique.')
+
+# print('Enter 5 numbers..')
+# a = list()
+# for i in range(5):
+#     a.append(int(input('Enter: ')))
+#     print(['Unique.', 'DUPLICATES.'][len(set(a))!= len(a)])
+
+
