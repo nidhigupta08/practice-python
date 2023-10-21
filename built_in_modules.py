1bc33a2d7e89be67152472df11e3d28bd3a5e802
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-10-21 18:25:41 +0530

    college_time python

---
+++ built_in_modules.py
@@ -0,0 +1,23 @@
+# 1. 1) math and cmath modules
+# import math
+# print(math.ceil(1.001))
+
+# from math import*
+# print(ceil(1.002))
+# print(factorial(4))
+
+# 2) cmath
+# from cmath import*
+# c=2+2j
+# exp(c)
+# 2.  decimal module
+# 3.) fraction module
+# 4) static module
+# 5) time module
+# 6)datetime module
+# 7) calendar module
+import calendar
+yy=2016
+mm=2
+print(calendar.month(yy,mm))
+# 8)sys module
