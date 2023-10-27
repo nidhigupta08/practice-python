d50459a97c2b7bb2e6704332b1dccee845037272
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-10-27 23:06:40 +0530

    The math.fsum() method returns the sum of all items in any iterable (tuples, arrays, lists, etc.).

---
+++ math1.py
@@ -0,0 +1,19 @@
+# example of a math module in Python for calculating the GCD of two numbers.
+
+import math
+n1=int(input("enter a num1: "))
+n2=int(input("enter a num2: "))
+res=math.gcd(n1,n2)
+print(res)
+
+# E:\Python\python.exe C:/Users/Admin/PycharmProjects/practice_python/math1.py
+# 6
+# 8
+# 2
+
+# Import math Library
+
+# Print the sum of all items
+print(math.fsum([1, 2, 3, 4, 5]))
+print(math.fsum([100, 400, 340, 500]))
+print(math.fsum([1.7, 0.3, 1.5, 4.5]))
