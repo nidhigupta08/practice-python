117e24f0695ff29d0bba5c02617086d775722efc
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-10-27 23:08:17 +0530

    Python has a built-in math module. It is a standard module, so we do not need to install it separately. We only must import it into the program we want to use.

---
+++ math.py
@@ -0,0 +1,29 @@
+# Program Code 1:
+# This program will show the calculation of square root using the math module
+# importing the math module
+import math
+print(math.sqrt( 9 ))
+
+# Program code 2:
+import math
+n=int(input("enter a number: "))
+print(math.factorial(n))
+
+# Program code 3:
+
+import math
+num=int(input("enter a number"))
+exp=int(input())
+res=math.pow(num, exp)
+print(res)
+
+
+# result
+# E:\Python\python.exe C:/Users/Admin/PycharmProjects/practice_python/math.py
+# 3.0
+# enter a number: 4
+# 24
+# enter a number5
+# 7
+# 78125.0
+
