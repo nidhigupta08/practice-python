35db6ebe921b26c06bee95a8aaa6a0f88794f2ee
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-10-28 23:01:14 +0530

    The randint() Function. random.random()

---
+++ Random.py
@@ -0,0 +1,16 @@
+# Python program for generating random float number
+# The Python Random module is a built-in module for generating random integers in Python.
+# These numbers occur randomly and does not follow any rules or instructuctions.
+
+# The random.random() function gives a float number that ranges from 0.0 to 1.0.
+import random
+num=random.random()
+print(num)
+
+# The randint() Function
+# The random.randint() function generates a random integer from the range of numbers supplied.
+
+# Python program for generating a random integer
+import random
+num = random.randint(1, 500)
+print( num )
