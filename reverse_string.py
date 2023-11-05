4ec89cc99a161f0f2c0bb6f9146fbab3d209b32e
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-11-05 22:55:17 +0530

    reverse

---
+++ reverse_string.py
@@ -0,0 +1,8 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Example usage:
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print("Original string:", original_string)
+print("Reversed string:", reversed_string)
