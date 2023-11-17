3f52d9842ef6ee2654292f736b9a624a37efe58e
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-11-17 23:28:02 +0530

    programs

---
+++ operators.py
@@ -0,0 +1,18 @@
+# arithmetics operators
+print("5+6 is", 5+6)
+print("5-6 is", 5-6)
+print("5/6 is", 5/6)
+print("15//6 is", 15//6)  # Floor division means dividing
+# and rounding down to the nearest integer.
+print("5*6 is", 5*6)
+print("5**3 is", 5**3)   # 5 to the power 3
+
+# LOGICAL OPERATORS
+# AND OR NOT
+a = True
+b = False
+print(a and b)  # true is 0 and false os 1
+# return true if both thr statements are true(T AND T =T)
+
+# OR  -returns true if one of the statements are true(T OR F=T)
+# not-reverse the results, returns false if the results is true
