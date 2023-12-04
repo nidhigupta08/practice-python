c0240c6e2e1fb8c6d2cb9cdd9d04368bd9fc82ff
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-04 19:16:24 +0530

    example demonstrating casting and type conversion in Python

---
+++ casting.py
@@ -0,0 +1,19 @@
+# Casting to int
+num_str = "123"
+num_int = int(num_str)
+print(f"String '{num_str}' casted to int: {num_int}")  # Output: String '123' casted to int: 123
+
+# Casting to float
+float_str = "3.14"
+float_num = float(float_str)
+print(f"String '{float_str}' casted to float: {float_num}")  # Output: String '3.14' casted to float: 3.14
+
+# Casting to string
+value_int = 42
+value_str = str(value_int)
+print(f"Integer {value_int} casted to string: {value_str}")  # Output: Integer 42 casted to string: 42
+
+# Casting to bool
+bool_str = "True"
+bool_val = bool(bool_str)
+print(f"String '{bool_str}' casted to bool: {bool_val}")  # Output: String 'True' casted to bool: True
