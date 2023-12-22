02aaddfebd324718e243374ebd784a99f0b0ba6f
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-22 23:32:46 +0530

    small program

---
+++ user_defined_fun.py
@@ -0,0 +1,10 @@
+# # declaring a function
+# def fun():
+#     print("inside function")
+#
+#     # calling function
+# fun()
+
+def avg_num(x, y):
+    print("average of ",x,"and" ,y, "is", (x+y)/2)
+avg_num(5,5)
