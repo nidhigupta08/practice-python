af23a0ba639307abad27943fe306397cdbce52b8
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-24 22:42:03 +0530

    program

---
+++ A2setA2.py
@@ -0,0 +1,4 @@
+# Write a python script to count the number of characters (character frequency) in a string. Sample
+# String : google.com'. Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1
+
+var = str(input("Enter a string"))
