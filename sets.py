f021da265885e8db59a249831613f8d8b6bd6ef3
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-26 22:09:25 +0530

    program on set

---
+++ sets.py
@@ -0,0 +1,106 @@
+# s = set()
+# # print(type(s))
+# # s_from_list = set([1, 2, 3, 4, 5])
+# # print(s_from_list)
+# # print(type(s_from_list))
+# # l = [1, 2, 3, 4]
+# # s_from_list = set(l)
+# s.add(1)
+# s.add(2)
+# s.add(4)
+# # s.union({1, 2, 3, 4}) # union: common between two sets.
+# # s1 = s.union({1, 2, 3, 4})
+# # s1 = s.union({1, 2, 3, 4})
+# s1 = s.intersection({1, 2, 3, 4})
+# print(s)
+# print(s1)
+#
+""" if else statement
+
+var2 = 50
+var3 = int(input("Enter a number"))
+
+if var3 > var2:
+    print("entered number is greater ")
+if var3 == var2:
+    print("Equal")
+else:
+    print("Value is lesser.")
+"""
+"""
+# IF ELSE IF BUT IN PYTHON we use elif
+
+var1 = 50
+var2 = int(input("Enter a number:"))
+if var2 > var1:
+    print("The number is greater")
+elif var2 == var1:
+    print("Equal")
+else:
+    print("Entered number is lesser .")
+"""
+"""
+list1 = [1, 2, 3, 4, 5]
+# if 3 in list1:
+#     print("Yes, entered number is in the list")
+
+# print(5 in list1)  # return  true bcz 5 in list1
+# if 5 in list1:
+#     print("yes it is there.")
+
+# print(15 in list1)  # return  false bcz 15 is not in list1
+# if 15 in list1:
+#     print("no it is no there.")
+
+# print(15 not in list1)  # return  true bcz 15 is not in list1 which is correct.
+# if 15 not in list1:
+#     print("no it is no there.")
+    """
+# QUIZ
+# num1 = 18
+# num = int(input("Enter your age:"))
+# if num > num1:
+#     print("You can drive")
+# elif num == num1:
+#     print("You are exactly 18 years old so we cannot decide. you have to present physically")
+# else:
+#     print("Yor cannot drive")
+
+# num = int(input("Enter your age:"))
+# if num > 18:
+#     print("You can drive")
+# elif num == 18:
+#     print("You are exactly 18 years old so we cannot decide. you have to present physically")
+# else:
+#     print("Yor cannot drive")
+
+"""
+THIS IS WRONG SOLUTION
+
+# 45*3=555, 56+9= 77, 56/6= 4
+num1 = 555
+num2 = 77
+num3 = 4
+num = int(input("Enter number"))
+if num == 45:
+    if num * 3:
+        if num == num1:
+            print("45*3 = 555")
+elif num == 56:
+    if num + 9:
+        if num == num2:
+            print("56+9 =77")
+# elif num == 56/6:
+#     if num == num3:
+#         print("56/6 =4")
+else:
+    print("c")
+    """
+
+
+
+
+
+
+
+
