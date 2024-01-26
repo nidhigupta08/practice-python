379a0cdc30ee336c3e9cfa2818da7da7b463fc5f
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-26 22:11:01 +0530

    A simple program

---
+++ my_module.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    "this function add two integer value"
+    result=a+b
+    return result
+def mult(a,b):
+    "this will give multiplication value of 2 integer"
+    result=a*b
+    return result
+
