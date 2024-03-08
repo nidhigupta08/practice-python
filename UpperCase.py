f9fc62271272ab0e5cd7daadc451fef4b6096345
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-08 23:23:19 +0530

    Program to convert the text  into Upper Case

---
+++ UpperCase.py
@@ -0,0 +1,12 @@
+def main():
+    # Ask the user for text input
+    text = input("Enter some text: ")
+
+    # Convert the text to uppercase
+    uppercase_text = text.upper()
+
+    # Print the uppercase text
+    print("Uppercase text:", uppercase_text)
+
+if __name__ == "__main__":
+    main()
