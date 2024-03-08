2c389c458db307c525f1b89998c84f868140615e
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-08 23:22:51 +0530

    Program to convert the text  into Lower Case

---
+++ LowerCase.py
@@ -0,0 +1,12 @@
+def main():
+    # Ask the user for text input
+    text = input("Enter some text: ")
+
+    # Convert the text to lowercase
+    lowercase_text = text.lower()
+
+    # Print the lowercase text
+    print("Lowercase text:", lowercase_text)
+
+if __name__ == "__main__":
+    main()
