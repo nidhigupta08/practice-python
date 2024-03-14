b4cf862189756e0c590fbe2188c89a8d05a08c04
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-14 22:20:14 +0530

    program

---
+++ incrementProgram.py
@@ -0,0 +1,15 @@
+# Function to increment a variable
+def increment(x):
+    return x + 1
+
+# Function to decrement a variable
+def decrement(x):
+    return x - 1
+
+# Example usage
+num = 5
+num = increment(num)
+print(num)  # Output: 6
+
+num = decrement(num)
+print(num)  # Output: 5
