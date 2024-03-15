fdf58e97b56c659cfa1efbff3130bbbcdbb5b9cf
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-15 23:29:17 +0530

    pattern program using Python to print a simple triangle pattern

---
+++ patternDiamond.py
@@ -0,0 +1,10 @@
+def print_diamond(rows):
+    for i in range(1, rows + 1):
+        print(" " * (rows - i) + "* " * i)
+
+    for i in range(rows - 1, 0, -1):
+        print(" " * (rows - i) + "* " * i)
+
+
+rows = int(input("Enter the number of rows for the diamond: "))
+print_diamond(rows)
