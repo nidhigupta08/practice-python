e48693a575fbd62bc78e423e285ea6e18227cbe6
Nidhi <guptanidhi8000@gmail.com>
2024-04-12 23:48:08 +0530

    program to print hollow rectangle program

---
+++ print_hollow_rectangle.py
@@ -0,0 +1,12 @@
+def print_hollow_rectangle(rows, cols):
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+
+rows = int(input("Enter the number of rows for the rectangle: "))
+cols = int(input("Enter the number of columns for the rectangle: "))
+print_hollow_rectangle(rows, cols)
