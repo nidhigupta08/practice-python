85fa43530c362dd5062665af888e37e50c4e8313
Nidhi <guptanidhi8000@gmail.com>
2024-06-01 23:10:09 +0530

    Using f-Strings:    - Demonstrates embedding expressions inside string literals using f-strings.    - Variables are directly included in the string using curly braces {}

---
+++ Usingf-Strings.py
@@ -0,0 +1,10 @@
+# Program 1: Using f-Strings
+
+name = "Alice"
+age = 30
+height = 1.75
+
+# Using f-strings to format the string
+formatted_string = f"My name is {name}, I am {age} years old and my height is {height} meters."
+
+print(formatted_string)
