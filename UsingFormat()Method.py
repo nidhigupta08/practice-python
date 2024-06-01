85fe32246a1512a7078f8c3db52fc1a0a1d2f378
Nidhi <guptanidhi8000@gmail.com>
2024-06-01 23:10:36 +0530

    Using the format() Method:    - Shows how to use the format() method to insert values into a string.    - Variables are passed to the format() method, replacing placeholders {}

---
+++ UsingFormat()Method.py
@@ -0,0 +1,10 @@
+# Program 2: Using the format() Method
+
+name = "Bob"
+age = 25
+height = 1.80
+
+# Using the format() method to format the string
+formatted_string = "My name is {}, I am {} years old and my height is {} meters.".format(name, age, height)
+
+print(formatted_string)
